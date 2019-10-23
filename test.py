77e627d21e56ac285a2ad404cd3c63a904026a1e
jinhongyi <3231950289@qq.com>
2019-10-23 22:16:48 +0800

    add title for all tests

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 if os.path.exists("temp"):
     os.system("rm -rf ./temp")
 os.makedirs("temp")
-for i in range(53):
+for i in range(54):
     print("./python_interpreter "+str(i)+" > temp/test"+str(i)+".out")
     os.system("./python_interpreter "+str(i)+" > temp/test"+str(i)+".out")
     if not filecmp.cmp("testData/test"+str(i)+".out","temp/test"+str(i)+".out"):
